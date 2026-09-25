70a1726f00d15c2934ebb60a4acb1a269b411b93
RajatK123-tec <111166952+RajatK123-tec@users.noreply.github.com>
2026-09-25 10:56:25 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Maven Jenkins Lab!");
         System.out.println("Hello World!");
+        System.out.println("Hello Jenkins");
     }
 }
